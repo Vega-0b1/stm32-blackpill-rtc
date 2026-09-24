5c1426e07b1873ec3abafeeca96cae5a94c4861b
jcvega <jcvega0b1@gmail.com>
2026-09-24 09:53:51 -0700

    sync to finish aht20.c

--- Core/Src/aht20.c
+++ Core/Src/aht20.c
@@ -6,4 +6,12 @@ static I2C_HandleTypeDef *i2c_handle;
 aht20_status_t aht20_init(I2C_HandleTypeDef *hi2c) {
   i2c_handle = hi2c;
   HAL_Delay(100);
+  uint8_t status;
+  if(HAL_I2C_Master_Receive(i2c_handle, AHT20_ADDR, &status, 1, 100) != HAL_OK)
+    return AHT20_ERR_I2C;
+
+  if((status & 0x18) != 0x18)
+    return AHT20_ERR_UNCALIBRATED;
+
+  return AHT20_OK;
 }
